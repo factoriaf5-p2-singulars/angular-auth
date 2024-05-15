be9e5b4eb18b0eced542c27cb801505edf1a6665
Raul <you@example.com>
2024-05-15 13:02:34 +0200

    add user service and conver user profile response to promise

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -1,18 +1,24 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
+import { UserService } from '../shared/services/user.service';
+import { Observable } from 'rxjs';
+import { User } from '../shared/models/user';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, JsonPipe],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent implements OnInit {
 @Input() id!:string;
+private userService = inject(UserService);
 
-ngOnInit(): void {
+user:any;
+async ngOnInit() {
   console.log(this.id);
-  
+  this.user =await this.userService.getUserById(this.id);
 }
 
 }
